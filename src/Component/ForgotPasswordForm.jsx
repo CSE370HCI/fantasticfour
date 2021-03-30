f45ca69387d0c5f4329f94952751ab49518451b0
Nuhash Chowdhury <nuhashchowdhury@outlook.com>
2021-03-30 14:47:11 -0400

    #62 - add alternate form in same component for after sending the token to the email address

--- src/Component/ForgotPasswordForm.jsx
+++ src/Component/ForgotPasswordForm.jsx
@@ -5,29 +5,50 @@ class ForgotPasswordForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          profile_picture: "",
-          username: "",
-          email: ""
+          email: "",
+          tokensent: false
         };
-        this.forgotPasswordForm = React.createRef();
     }
 
 
     render() {
-        return (
-            <div>
-                <h1>Forgot your password?</h1>
-                <p>Enter your e-mail to reset your password</p>
-                <form onSubmit={this.submitHandler}>
-                    <label>
-                        E-mail
-                        <input type="email"/>
-                    </label>
-                    <br/>
-                    <input type="submit" value="Reset Password"/>
-                </form>
-            </div>
-        );
+        if (!this.state.tokensent) {
+            return (
+                <div>
+                    <h1>Forgot your password?</h1>
+                    <p>Enter your e-mail to reset your password</p>
+                    <form onSubmit={this.submitHandler}>
+                        <label>
+                            E-mail
+                            <input type="email"/>
+                        </label>
+                        <br/>
+                        <input type="submit" value="Reset Password"/>
+                    </form>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div>
+                    <h1>Reset Password</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <label>
+                            Enter new password
+                            <input type="password" name="password"/>
+                        </label>
+                        <br/>
+                        <label>
+                            Confirm new password
+                            <input type="password" name="confirmPassword"/>
+                        </label>
+                        <br/>
+                        <input type="submit" value="Create New Password"/>
+                    </form>
+                </div>
+            )
+        }
+        
     }
 }
 
