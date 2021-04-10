a6f7e3f24b83c8fe703c2ab505a2dcbaa1972bac
ztlaistn <ztlaistn@buffalo.edu>
2021-04-09 23:03:50 -0400

    Added A/B Test
    -A includes a notification in the comments section when a user isn't signed in.
    
    -B simply relies on the user not having buttons for posting comments if they aren't signed in.

---
+++ src/Component/PostingListB.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import PostB from "./PostB.jsx";
+import "./styles/PostingList.css";
+
+export default class PostingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      posts: [],
+      listType: props.listType
+    };
+    this.postingList = React.createRef();
+    this.loadPosts = this.loadPosts.bind(this);
+  }
+
+  componentDidMount() {
+
+    this.loadPosts();
+
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log("PrevProps "+prevProps.refresh);
+    console.log("Props "+this.props.refresh);
+    if (prevProps.refresh !== this.props.refresh){
+      this.loadPosts();
+    }
+  }
+
+  loadPosts() {
+    let url = process.env.REACT_APP_API_PATH+"/posts?parentID=";
+    if (this.props && this.props.parentid){
+      url += this.props.parentid;
+    }
+    fetch(url, {
+      method: "get",
+      headers: {
+        'Content-Type': 'application/json'
+
+      },
+
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          if (result) {
+            this.setState({
+              isLoaded: true,
+              posts: result[0]
+            });
+            console.log("Got Posts");
+          }
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+          console.log("ERROR loading Posts")
+        }
+      );
+  }
+
+  render() {
+    //this.loadPosts();
+    const {error, isLoaded, posts} = this.state;
+    if (error) {
+      return <div> Error: {error.message} </div>;
+    } else if (!isLoaded) {
+      return <div> Loading... </div>;
+    } else if (posts) {
+
+      if (posts.length > 0){
+      return (
+
+        <div className="posts">
+
+          {posts.map(post => (
+            <PostB key={post.id} post={post} type={this.props.type} loadPosts={this.loadPosts}/>
+          ))}
+
+        </div>
+
+      );
+      } else {
+          return (<div> No Posts Found </div>);
+        }
+    } else {
+        return <div> *cricket noises (no posts yet)* </div>;
+      }
+  }
+}
